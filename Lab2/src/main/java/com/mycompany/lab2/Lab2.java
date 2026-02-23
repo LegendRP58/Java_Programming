8e987ad1eaf26a2bcc29b547c3a39fb686560614
Dmitry_ <dmitrybataickin@gmail.com>
2026-02-23 15:01:15 +0300

    LinkedList added

---
+++ Lab2/src/main/java/com/mycompany/lab2/Lab2.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.lab2;
+
+public class Lab2 {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new  Lab2JFrame().setVisible(true);
+        });
+    }
+}
